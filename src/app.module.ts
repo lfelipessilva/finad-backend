4ca52b3bbb806b410ef0aec73de4bc5588d3e224
Luis Felipe <lfelipe.silva@aterapia.com.br>
2022-06-10 19:38:22 -0300

    and .env files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config'
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [ConfigModule.forRoot({ envFilePath: `.env.${process.env.NODE_ENV}` }), UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
