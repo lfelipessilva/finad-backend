356a4ef7d4ff561400818e3d3a6340370ad02010
Luis Felipe <lfelipe.silva@aterapia.com.br>
2022-07-06 22:52:48 -0300

    add description do prisma schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
+import { IncomeModule } from './income/income.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ envFilePath: `.env.${process.env.NODE_ENV}` }),
     UserModule,
+    IncomeModule,
   ],
   controllers: [AppController],
   providers: [AppService],
