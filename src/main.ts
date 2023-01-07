e3b0df1a788055cc4a2622f8542df323dd3fec80
Luís Felipe <lfelipe.silva@aterapia.com.br>
2023-01-07 11:48:01 -0300

    try changing cors options

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: 'https://finad.devluis.tech' });
+  app.enableCors({ origin: 'https://finad.devluis.tech', methods: ['GET',  'POST'] });
   await app.listen(8888);
 }
 bootstrap();
