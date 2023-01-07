57280fc2d34a0c4064cc7ddfd7200a5d4993082a
Luis Felipe <luis.felipeszsv@gmail.com>
2023-01-07 20:23:49 -0300

    add urlencoded

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { json, urlencoded } from 'express'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: { 
+  const app = await NestFactory.create(AppModule);
+
+  app.use(json())
+  app.use(urlencoded({ extended: true }))
+
+  app.enableCors({ 
     origin: 'https://finad.devluis.tech',
-    allowedHeaders: [
-      'Content-Type',
-    ],
     credentials: true,
-  } });
+  });
 
   await app.listen(process.env.PORT || 8888);
 }
