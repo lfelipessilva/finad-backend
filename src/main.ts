3a87586e70f562654873a13e38784ae8c0452b62
Luis Felipe <luis.felipeszsv@gmail.com>
2023-01-07 20:43:37 -0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
 
   app.enableCors({ 
     origin: 'https://finad.devluis.tech',
-    allowedHeaders: ['Access-Control-Request-Headers']
+    allowedHeaders: ['Access-Control-Request-Headers', 'Content-Type']
   });
 
   await app.listen(process.env.PORT || 8888);
