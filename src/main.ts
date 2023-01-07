267bfa3a8b183799c574ccc57e93c733febccf36
Luis Felipe <luis.felipeszsv@gmail.com>
2023-01-07 17:41:02 -0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://finad.devluis.tech');
+    req.header('Access-Control-Allow-Origin', 'https://finad.devluis.tech');
     next();
   });
 
