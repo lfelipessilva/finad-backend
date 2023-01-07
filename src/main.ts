4f090ea247dd7732d98210291a60f8012198cb32
Luís Felipe <lfelipe.silva@aterapia.com.br>
2023-01-06 23:24:07 -0300

    add configured cors

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: '*'}); 
+  app.enableCors({ origin: 'https://finad.devluis.tech'}); 
   await app.listen(8888);
 }
 bootstrap();
