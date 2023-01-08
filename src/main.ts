e3b414cde527f8d40d3ff4d1576a4bbe63f893d9
Luis Felipe <luis.felipeszsv@gmail.com>
2023-01-08 18:16:04 -0300

    add frontend url to env variables

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'https://finad.devluis.tech',
+    origin: process.env.FRONT_END_URL,
     allowedHeaders: ['Content-Type'],
     methods: ['GET', 'POST', 'DELETE', 'OPTIONS'],
   });
