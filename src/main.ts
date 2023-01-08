d43114144ea75612e92c630def7034110ce177a6
Luis Felipe <luis.felipeszsv@gmail.com>
2023-01-07 22:44:39 -0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
 
   app.enableCors({
     allowedHeaders: ['content-type'],
-    origin: 'http://localhost:3000',
+    origin: 'https://finad.devluis.tech',
   });
 
   // app.use(
