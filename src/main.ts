ac3835155f066e8d9aa54e6b89a80370c0f58919
Luis Felipe <luis.felipeszsv@gmail.com>
2023-01-07 23:24:19 -0300

    add options to cors allowed methods

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
 
   app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
     next();
   });
