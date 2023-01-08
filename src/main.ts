5a56f507b989cfefeb12ced60beed1b8094b04a2
Luis Felipe <luis.felipeszsv@gmail.com>
2023-01-08 16:28:09 -0300

    add better cors configuration

--- src/main.ts
+++ src/main.ts
@@ -5,24 +5,11 @@ import { json, urlencoded } from 'express';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://finad.devluis.tech');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
-    next();
+  app.enableCors({
+    origin: 'https://finad.devluis.tech',
+    allowedHeaders: ['Content-Type'],
+    methods: ['GET', 'POST', 'DELETE', 'OPTIONS']
   });
-
-  app.enableCors();
-
-  // app.use(
-  //   json({
-  //     type: ['application/json', 'text/plain'],
-  //   }),
-  // );
-
-  app.use(json())
-  app.use(urlencoded({ extended: true }));
-
 
   await app.listen(process.env.PORT || 8888);
 }
