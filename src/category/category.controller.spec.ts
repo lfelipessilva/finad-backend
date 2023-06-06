3240c7d41042c022ba6dafc3418a06733f3ec7b5
Luis Felipe <luis.felipeszsv@gmail.com>
2023-06-06 02:14:02 -0300

    test: fix category tests

--- src/category/category.controller.spec.ts
+++ src/category/category.controller.spec.ts
@@ -1,12 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CategoryController } from './category.controller';
 import { CategoryService } from './category.service';
+import { PrismaModule } from '../../prisma/prisma.module';
 
 describe('CategoryController', () => {
   let controller: CategoryController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [PrismaModule],
       controllers: [CategoryController],
       providers: [CategoryService],
     }).compile();
--- src/category/category.service.spec.ts
+++ src/category/category.service.spec.ts
@@ -1,11 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CategoryService } from './category.service';
+import { PrismaModule } from '../../prisma/prisma.module';
 
 describe('CategoryService', () => {
   let service: CategoryService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [PrismaModule],
       providers: [CategoryService],
     }).compile();
 
