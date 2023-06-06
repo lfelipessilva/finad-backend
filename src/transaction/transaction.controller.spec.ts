3d5ad72b1a58b435b4f39acf0f6dbcc761107c3e
Luis Felipe <luis.felipeszsv@gmail.com>
2023-06-06 02:12:52 -0300

    test: fix transaction tests

--- src/transaction/transaction.controller.spec.ts
+++ src/transaction/transaction.controller.spec.ts
@@ -1,12 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TransactionController } from './transaction.controller';
 import { TransactionService } from './transaction.service';
+import { PrismaModule } from '../../prisma/prisma.module';
 
 describe('TransactionController', () => {
   let controller: TransactionController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [PrismaModule],
       controllers: [TransactionController],
       providers: [TransactionService],
     }).compile();
--- src/transaction/transaction.service.spec.ts
+++ src/transaction/transaction.service.spec.ts
@@ -1,11 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TransactionService } from './transaction.service';
+import { PrismaModule } from '../../prisma/prisma.module';
 
 describe('TransactionService', () => {
   let service: TransactionService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [PrismaModule],
       providers: [TransactionService],
     }).compile();
 
