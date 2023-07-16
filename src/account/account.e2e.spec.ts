4cf34f441227a867da1ac57693e6d06fed546d58
Luis Felipe <luis.felipeszsv@gmail.com>
2023-07-15 22:32:29 -0300

    add account e2e

---
+++ src/account/account.e2e.spec.ts
@@ -0,0 +1,91 @@
+import { INestApplication } from '@nestjs/common';
+import * as supertest from 'supertest';
+import { Test, TestingModule } from '@nestjs/testing';
+import { AccountController } from './account.controller';
+import { AccountService } from './account.service';
+import { Account } from '@prisma/client';
+import { CreateAccountDTO } from './dto/create-account.dto';
+import { UserService } from '../user/user.service';
+import { PrismaService } from '../../prisma/prisma.service';
+import { randomUUID } from 'crypto';
+import { faker } from '@faker-js/faker';
+import { PrismaModule } from '../../prisma/prisma.module';
+import { AuthModule } from '../auth/auth.module';
+
+describe('AccountController', () => {
+  let accountController: AccountController;
+  let prismaService: PrismaService;
+  let userService: UserService;
+  let app: INestApplication;
+
+  beforeAll(() => {
+    prismaService = new PrismaService();
+    userService = new UserService(prismaService);
+
+    prismaService.user.deleteMany()
+  })
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [PrismaModule, AuthModule],
+      controllers: [AccountController],
+      providers: [AccountService],
+    }).compile();
+
+    accountController = module.get<AccountController>(AccountController);
+
+    app = module.createNestApplication();
+    await app.init();
+  });
+
+  it('should be defined', () => {
+    expect(accountController).toBeDefined();
+  });
+
+  it('should not create account with undefined user', async () => {
+    const account: CreateAccountDTO = {
+      userId: 'no user',
+      name: 'test',
+      description: 'for testing purposes',
+    }
+
+    const response = await supertest(app.getHttpServer())
+      .post('/account')
+      .expect(200)
+      .send(account)
+
+    console.log(response)
+    expect(response.status).toBe(400)
+  })
+
+  it('should create an account', async () => {
+    const user = await userService.create({
+      id: randomUUID(),
+      name: faker.person.firstName(),
+      email: faker.internet.email(),
+      money: 0,
+      password: faker.internet.password(),
+      created_at: new Date(),
+      updated_at: new Date(),
+    })
+
+    const account: CreateAccountDTO = {
+      userId: user.id,
+      name: 'test',
+      description: 'for testing purposes',
+    }
+
+    return supertest(app.getHttpServer())
+      .post('/account')
+      .expect(200)
+      .send(account)
+    // const response: any = accountController.create(account)
+    //  
+    // expect(response.data).toHaveProperty('id')
+    // expect(response.status).toBe(201)
+  });
+
+  afterAll(() => {
+    prismaService.user.deleteMany()
+  })
+});
